383e35398561ff82caa901d178574ae1add98abc
garotogordo <garotogordo@icloud.com>
2022-03-11 16:27:32 -0300

    Revert "Updates"
    This reverts commit 6ea69399b0a35e347422c05b24a106f47f3a43db.

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const displayName = "Yuri Cunha"; // Mostrar nome
+const bannerDescription = "Database Administrator (dbA)"; // Descrição do banner
+const mediumUrl =
+  "https://api.allorigins.win/raw?url=https://medium.com/feed/@garotoGordo"; // URL do feed do Medium
+const mediumFilterKeyword = ""; // Palavra-chave para filtrar o feed do Medium
+const email = "garotogordo@icloud.com"; // E-mail
+const githubUrl = "https://github.com/garotogordo"; // URL do GitHub
+const linkedInUrl = "https://www.linkedin.com/in/ycdpp/"; // URL do LinkedIn
+const resumePdfTitle = "Yuri-Cunha.pdf"; // Título do PDF do currículo
+
+const projects = [
+  {
+    title: "Auto GitHub follow e unfollow.",
+    description:
+      "Seguir e deixar de seguir automaticamente usuários do GitHub.",
+    stack: ["Javascript"],
+    imgName: "follow__unfollow.png",
+    siteUrl: "https://github.com/garotogordo/Auto-Github-Follow-e-Unfollow",
+    codeUrl: "https://github.com/garotogordo/Auto-Github-Follow-e-Unfollow",
+  },
+  {
+    title: "SongDedi [Colaboração]",
+    description:
+      "Um serviço gratuito de dedicação de música adequado para casamentos, festas ou qualquer evento.",
+    stack: ["Vue.js", "Python Flask", "Firebase", "Google Analytics"],
+    imgName: "songdedi.png",
+    siteUrl: "https://songdedi-admin.herokuapp.com/",
+    codeUrl: "https://github.com/huishun98/songdedi-admin",
+    altCodeUrl: "https://github.com/huishun98/songdedi-requests",
+  },
+  {
+    title: "Galera/Galeria de Projeto",
+    description:
+      "Um site semelhante à uma galeria, para colocar links diretos a seus projetos.",
+    stack: ["PHP", "CSS"],
+    imgName: "galeria_de_projetos.jpg",
+    siteUrl: "https://github.com/garotogordo/Auto-Github-Follow-e-Unfollow",
+    codeUrl: "https://github.com/garotogordo/Auto-Github-Follow-e-Unfollow",
+  },
+];
+
+export {
+  projects,
+  mediumUrl,
+  mediumFilterKeyword,
+  email,
+  githubUrl,
+  linkedInUrl,
+  displayName,
+  bannerDescription,
+  resumePdfTitle,
+};
